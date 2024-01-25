3d2f5e830426fa8ab24f4b881b710d4c742193eb
Amir Barra <barra.amir14@gmail.com>
2024-01-24 22:40:27 -0400

    task/02: Creacion de parte visual login Organizador

--- src/app/modules/users/pages/login/login.component.ts
+++ src/app/modules/users/pages/login/login.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { OrganizerLoginComponent } from '../../components/organizer-login/organizer-login.component';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
