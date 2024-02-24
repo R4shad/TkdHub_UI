4dba295030d441c9310ddfb8f66bf1cd57657257
Amir Barra <barra.amir14@gmail.com>
2024-02-24 10:10:49 -0400

    task/21: Correccion cambios con competitor, creacion y muetras de agrupaciones

--- src/app/modules/admin/pages/grouping-competitors/grouping-competitors.component.ts
+++ src/app/modules/admin/pages/grouping-competitors/grouping-competitors.component.ts
@@ -30,16 +30,13 @@ import {
   styleUrls: ['./grouping-competitors.component.scss'],
 })
 export class GroupingCompetitorsComponent implements OnInit {
-  competitors: completeCompetitorI[] = [];
-  clubs: clubI[] = [];
-  competitorsFilter: completeCompetitorI[] = [];
-  genterFilter = new FormControl('');
-  clubFilter = new FormControl('');
   championshipId: number = 0;
-  clubCode: string = '';
 
-  championshipCategories: categoryI[] = [];
-  championshipDivisions: divisionI[] = [];
+  competitors: completeCompetitorI[] = [];
+
+  categories: categoryI[] = [];
+  divisions: divisionI[] = [];
+
   brackets: bracketWithCompetitorsI[] = [];
   constructor(
     private api: ApiService,
@@ -48,90 +45,52 @@ export class GroupingCompetitorsComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    console.log(this.brackets);
-    console.log(this.brackets.length);
     this.route.paramMap.subscribe((params) => {
       this.championshipId = Number(params.get('championshipId'));
     });
-    this.getClubs();
-    this.displayCompetitors();
 
-    this.getApiDivisions();
-    this.getApiCategories();
-    this.getBrackets();
+    this.getData();
   }
 
-  getCurrentRoute(): string {
-    return this.route.snapshot.url.map((segment) => segment.path).join('/');
-  }
-
-  displayCompetitors() {
+  getData() {
     this.api
       .getChampionshipCompetitors(this.championshipId)
       .subscribe((data) => {
-        console.log(data);
         this.competitors = data;
-        this.competitorsFilter = data;
       });
-  }
 
-  getClubs() {
-    this.api.getClubs(this.championshipId).subscribe((data) => {
-      this.clubs = data;
-    });
-  }
-
-  getBrackets() {
     this.api
       .getBracketsWithCompetitors(this.championshipId)
       .subscribe((data) => {
         this.brackets = data;
         console.log(this.brackets);
       });
-  }
 
-  filterGender() {
-    const genderFilter = this.genterFilter.value;
-    console.log(genderFilter);
-    if (genderFilter === 'todos') {
-      this.competitorsFilter = this.competitors;
-    } else {
-      // Filtrar inscritos por sexo
-      this.competitorsFilter = this.competitors.filter(
-        (competitor) => competitor.Participant.gender === genderFilter
-      );
-    }
-  }
+    this.api
+      .getCategoriesWithCompetitors(this.championshipId)
+      .subscribe((data) => {
+        this.categories = data;
+      });
 
-  filterClub() {
-    const clubFilter = this.clubFilter.value;
-    //console.log(clubFilter);
-    if (clubFilter === 'todos') {
-      this.competitorsFilter = this.competitors;
-    } else {
-      // Filtrar inscritos por sexo
-      this.competitorsFilter = this.competitors.filter(
-        (competitor) => competitor.Participant.clubCode === clubFilter
-      );
-    }
-  }
-
-  returnToSummary() {
-    this.router.navigate(['/championship', this.championshipId, 'Organizer']);
-  }
-
-  goToBracketDraw() {
-    this.router.navigate([
-      '/championship',
-      this.championshipId,
-      'Organizer',
-      'BracketDraw',
-    ]);
+    this.api
+      .getDivisionsWithCompetitors(this.championshipId)
+      .subscribe((data) => {
+        this.divisions = data;
+        for (const division of this.divisions) {
+          this.api.getDivisionData(division.divisionName).subscribe((data) => {
+            division.ageIntervalId = data.ageIntervalId;
+            division.gender = data.gender;
+            division.grouping = data.grouping;
+            division.minWeight = data.minWeight;
+            division.maxWeight = data.maxWeight;
+          });
+        }
+      });
   }
 
   createGroupings() {
-    for (const category of this.championshipCategories) {
-      for (const division of this.championshipDivisions) {
+    for (const category of this.categories) {
+      for (const division of this.divisions) {
         const competitorsInDivision = this.competitors.filter(
           (competitor) =>
             competitor.divisionName === division.divisionName &&
@@ -157,32 +116,8 @@ export class GroupingCompetitorsComponent implements OnInit {
     }
   }
 
-  getApiDivisions() {
-    this.api
-      .getDivisionsWithCompetitors(this.championshipId)
-      .subscribe((data) => {
-        this.championshipDivisions = data;
-
-        for (const division of this.championshipDivisions) {
-          this.api.getDivisionData(division.divisionName).subscribe((data) => {
-            division.ageIntervalId = data.ageIntervalId;
-            division.gender = data.gender;
-            division.grouping = data.grouping;
-            division.minWeight = data.minWeight;
-            division.maxWeight = data.maxWeight;
-          });
-        }
-      });
-  }
-  getApiCategories() {
-    this.api
-      .getCategoriesWithCompetitors(this.championshipId)
-      .subscribe((data) => {
-        this.championshipCategories = data;
-      });
-  }
   getBracketGrouping(bracket: bracketI) {
-    const matchingDivision = this.championshipDivisions.find(
+    const matchingDivision = this.divisions.find(
       (div) => div.divisionName === bracket.divisionName
     );
     if (matchingDivision) {
@@ -190,14 +125,35 @@ export class GroupingCompetitorsComponent implements OnInit {
     }
     return null;
   }
-
-  getBracketWeightInterval(bracket: bracketI) {
-    const matchingDivision = this.championshipDivisions.find(
+  getBracketMinWeightInterval(bracket: bracketI) {
+    const matchingDivision = this.divisions.find(
       (div) => div.divisionName === bracket.divisionName
     );
     if (matchingDivision) {
-      return matchingDivision.minWeight + '-' + matchingDivision.maxWeight;
+      return matchingDivision.minWeight;
     }
     return null;
+  }
+
+  getBracketMaxWeightInterval(bracket: bracketI) {
+    const matchingDivision = this.divisions.find(
+      (div) => div.divisionName === bracket.divisionName
+    );
+    if (matchingDivision) {
+      return matchingDivision.maxWeight;
+    }
+    return null;
+  }
+
+  returnToSummary() {
+    this.router.navigate(['/championship', this.championshipId, 'Organizer']);
+  }
+  goToBracketDraw() {
+    this.router.navigate([
+      '/championship',
+      this.championshipId,
+      'Organizer',
+      'BracketDraw',
+    ]);
   }
 }
