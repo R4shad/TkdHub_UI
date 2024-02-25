3b7f76d1ce2632dc23bb8f5f72f4ca6564d73f14
Amir Barra <barra.amir14@gmail.com>
2024-02-25 14:27:50 -0400

    task/22: Creacion eightParticipantsBracket

---
+++ src/app/modules/admin/components/eight-participants-bracket/eight-participants-bracket.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eight-participants-bracket',
+  templateUrl: './eight-participants-bracket.component.html',
+  styleUrls: ['./eight-participants-bracket.component.scss']
+})
+export class EightParticipantsBracketComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
