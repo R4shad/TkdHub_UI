c041b47a5d2d719bb73fdc8077126e2264bd7711
Amir Barra <barra.amir14@gmail.com>
2024-04-09 13:50:38 -0400

    task/41: Mejoras configuracion de campeonato

--- src/app/modules/users/pages/championship-view/championship-view.component.ts
+++ src/app/modules/users/pages/championship-view/championship-view.component.ts
@@ -28,8 +28,27 @@ export class ChampionshipViewComponent implements OnInit {
     });
   }
 
-  goToLogin() {
-    const championshipId = this.championship.championshipId;
-    this.router.navigate(['/championship', championshipId, 'login']);
+  formatFecha(fecha: string): string {
+    const meses = [
+      'enero',
+      'febrero',
+      'marzo',
+      'abril',
+      'mayo',
+      'junio',
+      'julio',
+      'agosto',
+      'septiembre',
+      'octubre',
+      'noviembre',
+      'diciembre',
+    ];
+
+    const fechaObj = new Date(fecha);
+    const dia = fechaObj.getDate();
+    const mes = meses[fechaObj.getMonth()];
+    const año = fechaObj.getFullYear();
+
+    return `${dia} de ${mes} del ${año}`;
   }
 }
--- src/app/modules/users/pages/main-page/main-page.component.ts
+++ src/app/modules/users/pages/main-page/main-page.component.ts
@@ -28,4 +28,28 @@ export class MainPageComponent implements OnInit {
     const url = `/championship/${championshipId}`;
     this.router.navigateByUrl(url);
   }
+
+  formatFecha(fecha: string): string {
+    const meses = [
+      'enero',
+      'febrero',
+      'marzo',
+      'abril',
+      'mayo',
+      'junio',
+      'julio',
+      'agosto',
+      'septiembre',
+      'octubre',
+      'noviembre',
+      'diciembre',
+    ];
+
+    const fechaObj = new Date(fecha);
+    const dia = fechaObj.getDate();
+    const mes = meses[fechaObj.getMonth()];
+    const año = fechaObj.getFullYear();
+
+    return `${dia} de ${mes} del ${año}`;
+  }
 }
